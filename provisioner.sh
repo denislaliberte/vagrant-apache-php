bcb2d2e3bee4239c91382618e2d9825dd86aac67
Denis Laliberté <denis@tp1.ca>
2015-11-11 15:44:22 -0500

    ET-9 add utils and vhost files

--- provisioner.sh
+++ provisioner.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "## install apache 2.4"
+echo '## Install utils'
+sudo yum install vim
+
+echo "## Install apache 2.4"
 cd /etc/yum.repos.d/
 sudo wget http://repos.fedorapeople.org/repos/jkaluza/httpd24/epel-httpd24.repo
 sudo yum install httpd24.x86_64 -y
 /opt/rh/httpd24/root/usr/sbin/httpd -version
+echo 'include /vagrant/etc/*.conf' | sudo tee -a /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
 service httpd24-httpd start
