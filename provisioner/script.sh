c559fd2c14891c10f430894570c6ec5d804eac8a
Denis Laliberté <denis@tp1.ca>
2015-11-18 17:03:24 -0500

    ET-9 use argument to choose php version

--- provisioner/script.sh
+++ provisioner/script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#PHP_REPO="--enablerepo=remi-php56"
-#PHP_REPO="--enablerepo=remi" ## php54
-PHP_REPO="" #php 5.3
-PHP_PACKAGE=php
+PHP_VERSION=$1
+
+if [[ "$PHP_VERSION" == "php53" ]]; then
+  PHP_REPO=""
+elif [[ "$PHP_VERSION" == "php54" ]]; then
+  PHP_REPO="--enablerepo=remi"
+else
+  PHP_REPO="--enablerepo=remi-php56"
+fi
+
 echo '## Install utils'
 sudo yum install vim -y
 
@@ -15,8 +21,8 @@ echo "## Install apache 2.4"
 sudo yum --enablerepo=remi install -y httpd 
 /opt/rh/httpd24/root/usr/sbin/httpd -version
 
-echo '## Install php 56'
-sudo yum $PHP_REPO  install -y $PHP_PACKAGE #php php-gd php-mysql php-mbstring php-xml
+echo '## Install $PHP_VERSION on repo $PHP_REPO'
+sudo yum $PHP_REPO  install -y php php-gd php-mysql php-mbstring php-xml
 
 echo 'include /vagrant/etc/*.conf' | sudo tee -a /etc/httpd/conf/httpd.conf
 service httpd start
