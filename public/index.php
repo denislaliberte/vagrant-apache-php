c0bf1781c1cd1fc9358015350bc32e7a4d2612e0
Denis Laliberté <denis@tp1.ca>
2015-11-25 17:37:26 -0500

    RBD-146 add test mysql

--- public/index.php
+++ public/index.php
@@ -1,8 +1,17 @@
 <h1>file index.php </h1>
 <p> php is activated : 
 <?php 
-      print "yes"; 
+      print "yes" . PHP_EOL; 
+      print "Mysql works :";
+      try {
+        $dbh = new PDO('mysql:host=172.16.10.92', 'tp1', 'pla514TY438pus');
+        if($dbh->query("show databases")->fetchColumn( 0 ) == "information_schema") echo "yes" . PHP_EOL;
+      } catch(PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+      }
       phpinfo();
+
       die("");
 ?>
 no
