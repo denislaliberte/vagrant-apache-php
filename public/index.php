67c2bc4190c4302137d86ff3123e54ae219461d7
Denis Laliberté <denis@tp1.ca>
2015-12-07 15:33:13 -0500

    ET-6 fix typo

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
       print "yes" . PHP_EOL; 
       print "Mysql works :";
       try {
-        $dbh = new PDO('mysql:host=10.0.0.1', 'tp1', 'pla514TY438pus');
+        $dbh = new PDO('mysql:host=10.0.0.1', 'root', '');
         if($dbh->query("show databases")->fetchColumn( 0 ) == "information_schema") echo "yes" . PHP_EOL;
       } catch(PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
