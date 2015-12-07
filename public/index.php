737d4c7a5f1a56c467a7e096beddab97c10e16bb
Denis Laliberté <denis@tp1.ca>
2015-12-07 15:30:10 -0500

    add host adresse for db

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
       print "yes" . PHP_EOL; 
       print "Mysql works :";
       try {
-        $dbh = new PDO('mysql:host=172.16.10.92', 'tp1', 'pla514TY438pus');
+        $dbh = new PDO('mysql:host=10.0.0.1', 'tp1', 'pla514TY438pus');
         if($dbh->query("show databases")->fetchColumn( 0 ) == "information_schema") echo "yes" . PHP_EOL;
       } catch(PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
